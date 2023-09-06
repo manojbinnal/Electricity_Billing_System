e85e0144013c56294c5de5152026efb558410871
manoj_binnal <109895471+manojbinnal@users.noreply.github.com>
2023-09-06 23:02:01 +0530

    change in db

--- src/Java_classes/Conn.java
+++ src/Java_classes/Conn.java
@@ -8,7 +8,7 @@ public class Conn{
     public Conn(){  
         try{  
             Class.forName("com.mysql.jdbc.Driver");  // method for establishing the connetion with JDBC driver (mysql-connection-java .jar)
-            c =DriverManager.getConnection("jdbc:mysql:///ebs","root","mysql123");   //root by default and database is ebs
+             c =DriverManager.getConnection("jdbc:mysql://localhost:3306/prodemo","root","Manojbinn@l01");   //root by default and database is ebs
             s =c.createStatement();    // create statement using the connection variable
             //now we have statemnt variable ready
             //now our connections with the JDBC mysql driver is setup with our database mySQL
